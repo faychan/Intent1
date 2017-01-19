6990ac49f01589754be301d832c9ac0b44531fd2
Faychan <nor.iffat32@gmail.com>
2017-01-19 20:19:35 +0700

    - startActivity Part 2

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/HasilActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/HasilActivity.java
@@ -1,8 +1,10 @@
 package id.sch.smktelkom_mlg.learn.intent1;
 
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class HasilActivity extends AppCompatActivity {
 
@@ -11,13 +13,22 @@ public class HasilActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hasil);
 
+        setTitle("Result");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        }
+
+        String nama = getIntent().getStringExtra(MainActivity.NAME);
+        int umur = getIntent().getIntExtra(MainActivity.AGE, 0);
+        int yearNow = Calendar.getInstance().get(Calendar.YEAR);
+        int tahunLahir = yearNow - umur;
+
+        TextView tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        tvHasil.setText(nama + " was born in " + tahunLahir);
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
-        onBackPressed();
+            onBackPressed();
             return true;
         }
         return super.onOptionsItemSelected(item);
--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/MainActivity.java
@@ -4,19 +4,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String NAME = "Name";
+    public static final String AGE = "Age";
+    EditText etNama;
+    EditText etUmur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.bHitung).setOnClickListener(new View.OnClickListener() {
+        setTitle("Input");
+
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        etUmur = (EditText) findViewById(R.id.editTextUmur);
+
+        findViewById(R.id.buttonHitung).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, HasilActivity.class));
+                goHasil();
             }
         });
+    }
+
+    private void goHasil() {
+        String nama = etNama.getText().toString();
+        String umurStr = etUmur.getText().toString();
+
+        int umur = umurStr.isEmpty() ? 0 : Integer.parseInt(umurStr);
+
+        Intent intent = new Intent(MainActivity.this, HasilActivity.class);
+        intent.putExtra(NAME, nama);
+        intent.putExtra(AGE, umur);
+
+        startActivity(intent);
     }
 }
